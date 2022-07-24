ced1c1983415f15075fcaa468738c55bb0f62d92
Omar Criacci <omarcriacci9@gmail.com>
2022-07-24 17:52:16 +0200

    add menu-dropdown on mobile

--- scripts/main.js
+++ scripts/main.js
@@ -6,13 +6,32 @@ function toggleMenu() {
 }
 
 function isMenuActive() {
-    //TODO
+    let menuContent = document.getElementById("menu-content")
+    return menuContent.style.height == "65px"
 }
 
 function closeMenu() {
-    //TODO
+    let menuContent = document.getElementById("menu-content")
+    let searchBar = document.getElementById("searchbar-mobile")
+    let searchBtn = document.getElementById("searchbtn-mobile")
+    let icon = document.querySelectorAll("#menu-button-mobile i")[0]
+
+    searchBar.style.display = "none"
+    searchBtn.style.display = "none"
+    menuContent.style.height = "0px"
+    icon.className = "bx bx-menu"
+    icon.style.fontSize = "36px"
 }
 
 function openMenu() {
-    //TODO
+    let menuContent = document.getElementById("menu-content")
+    let searchBar = document.getElementById("searchbar-mobile")
+    let searchBtn = document.getElementById("searchbtn-mobile")
+    let icon = document.querySelectorAll("#menu-button-mobile i")[0]
+
+    searchBar.style.display = "block"
+    searchBtn.style.display = "block"
+    menuContent.style.height = "65px"
+    icon.className = "bx bx-x"
+    icon.style.fontSize = "42px"
 }
